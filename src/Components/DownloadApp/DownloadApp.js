364ba5331edc80684ae1e879f1c4f9c164047520
shafiimam <shafi15-8072@diu.edu.bd>
2021-07-27 18:12:08 +0600

    done

--- src/Components/DownloadApp/DownloadApp.js
+++ src/Components/DownloadApp/DownloadApp.js
@@ -12,38 +12,35 @@ const DownloadApp = () => {
       <img src={redleaf2} className="redleaf2" /> */}
       <h1 className="main-text">How do customers gain access?</h1>
 
-      <div className="container"> 
-      <div className="grid-2">
-
-      <div className="left_section">
-              <div className="download">
-                <div className="qr_app">
-                  <img src={qr} className="qr" />
-                  <img src={appMobile} className="appMobile" />
-                </div>
+      <div className="container">
+        <div className="grid-2">
+          <div className="left_section">
+            <div className="download">
+              <div className="qr_app">
+                <img src={qr} className="qr" />
+                <img src={appMobile} className="appMobile" />
                 <img src={appstore} />
               </div>
-        </div>
+            </div>
+          </div>
 
-        <div className="righ_section">
-              <div className="info">
-                <div className="content">
-                  <h2>Download the stadion app</h2>
-                    <p>
-                      The QR code will direct customers straight into your personalised
-                      page, where they can view all your products.
-                    </p>
-                    <p>
-                      Customers purchase items on the app in a cashless and contactless
-                      manner.
-                    </p>
-                </div>
-                </div>
+          <div className="right_section">
+            <div className="info">
+              <div className="content">
+                <h2>Download the stadion app</h2>
+                <p>
+                  The QR code will direct customers straight into your
+                  personalised page, where they can view all your products.
+                </p>
+                <p>
+                  Customers purchase items on the app in a cashless and
+                  contactless manner.
+                </p>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
-
-
-      </div> 
     </div>
   );
 };
