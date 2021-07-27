a3c01f04bbb3dc565f6f7818933e7fc1e25c659f
Md Razu Ahammed Molla <razuahammed@icloud.com>
2021-07-27 11:53:44 +0600

    hero and navigate

--- src/Components/DownloadApp/DownloadApp.js
+++ src/Components/DownloadApp/DownloadApp.js
@@ -7,28 +7,43 @@ import redleaf2 from "../../Images/redleaf2.png";
 import "./DownloadApp.css";
 const DownloadApp = () => {
   return (
-    <div>
-      <img src={redleaf1} className="redleaf1" />
-      <img src={redleaf2} className="redleaf2" />
+    <div className="downloadSection">
+      {/* <img src={redleaf1} className="redleaf1" />
+      <img src={redleaf2} className="redleaf2" /> */}
       <h1 className="main-text">How do customers gain access?</h1>
-      <div className="downloadApp">
-        <div className="download">
-          <img src={qr} className="qr" />
-          <img src={appMobile} className="appMobile" />
-          <img src={appstore} />
+
+      <div className="container"> 
+      <div className="grid-2">
+
+      <div className="left_section">
+              <div className="download">
+                <div className="qr_app">
+                  <img src={qr} className="qr" />
+                  <img src={appMobile} className="appMobile" />
+                </div>
+                <img src={appstore} />
+              </div>
         </div>
-        <div className="info">
-          <h2>Download the stadion app</h2>
-          <p>
-            The QR code will direct customers straight into your personalised
-            page, where they can view all your products.
-          </p>
-          <p>
-            Customers purchase items on the app in a cashless and contactless
-            manner.
-          </p>
+
+        <div className="righ_section">
+              <div className="info">
+                <div className="content">
+                  <h2>Download the stadion app</h2>
+                    <p>
+                      The QR code will direct customers straight into your personalised
+                      page, where they can view all your products.
+                    </p>
+                    <p>
+                      Customers purchase items on the app in a cashless and contactless
+                      manner.
+                    </p>
+                </div>
+                </div>
         </div>
       </div>
+
+
+      </div> 
     </div>
   );
 };
--- src/Components/Hero/Hero.js
+++ src/Components/Hero/Hero.js
@@ -6,23 +6,51 @@ import leaf2 from "../../Images/leaf2.png";
 import "./hero.css";
 const Hero = () => {
   return (
+    <>
     <div className="hero">
-      <img src={dots} className="dots" />
-      <img src={leaf} className="leaf1" />
-      <img src={leaf2} className="leaf2" />
-      <div className="hero-title">
-        <h1>Mobile Ordering</h1>
-        <h1>Platform For Food</h1>
-        <p>
-          Our Mission is to enhance your customers experience, by makinh it easy
-          to order and re-order. No need to queue up
-        </p>
-        <button className="demo-button">Free demo</button>
-      </div>
-      <div className="hero-image">
-        <img src={hero} />
+      
+       <img src={dots} className="dots" />
+       <img src={leaf} className="leaf1" />
+       <img src={leaf2} className="leaf2" />
+
+      <div className="container">
+        <div className="grid-2">
+          <div className="left_section">
+            <div className="content">
+                <h1>Mobile Ordering</h1>
+                <h1>Platform For Food</h1>
+                <p>
+                  Our Mission is to enhance your customers experience, by makinh it easy
+                  to order and re-order. No need to queue up
+                </p>
+                <button className="demo-button">Free demo</button>
+            </div>
+          </div>
+
+          <div className="righ_section">
+           <img src={hero} />
+          </div>
+        </div>
       </div>
     </div>
+    </>
+    // <div className="hero">
+    //   <img src={dots} className="dots" />
+    //   <img src={leaf} className="leaf1" />
+    //   <img src={leaf2} className="leaf2" />
+    //   <div className="hero-title">
+    //     <h1>Mobile Ordering</h1>
+    //     <h1>Platform For Food</h1>
+    //     <p>
+    //       Our Mission is to enhance your customers experience, by makinh it easy
+    //       to order and re-order. No need to queue up
+    //     </p>
+    //     <button className="demo-button">Free demo</button>
+    //   </div>
+    //   <div className="hero-image">
+    //     <img src={hero} />
+    //   </div>
+    // </div>
   );
 };
 
